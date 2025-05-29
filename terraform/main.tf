ca29be67fb3edacacb4d155015cb313d6b0e56c3
alejolonber <alejandrolonber25@gmail.com>
2025-05-29 12:41:55 -0500

    fix: init.yml  fixed

--- terraform/main.tf
+++ terraform/main.tf
@@ -12,7 +12,6 @@ resource "azurerm_container_registry" "microservices_acr" {
   resource_group_name = azurerm_resource_group.microservices_rg.name
   location            = azurerm_resource_group.microservices_rg.location
   sku                  = "Basic"
-  admin_enabled       = true
 }
 
 module "aks" {
